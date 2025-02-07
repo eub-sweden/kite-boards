975daa1329569832ef6ac12d63d8d6d843fb4358
Benjamin Lindqvist <benjamin@eub.se>
2025-02-07 13:23:00 +0100

    add kite_nrf9151 board files

---
+++ boards/arm/kite_nrf9151/board.cmake
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+
+if(CONFIG_BOARD_KITE_NRF9151_NS)
+  set(TFM_PUBLIC_KEY_FORMAT "full")
+endif()
+
+if(CONFIG_TFM_FLASH_MERGED_BINARY)
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+endif()
+
+# FIXME: Does this even work
+board_runner_args(jlink "--device=nRF9161_xxCA" "--speed=4000")
+board_runner_args(pyocd "--target=nrf9160_xxaa" "--frequency=4000000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
