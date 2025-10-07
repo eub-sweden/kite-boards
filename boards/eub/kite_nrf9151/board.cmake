447e77a34457a7144735b927a035be295185eca8
Viktor Sjölind <viktor@eub.se>
2025-10-07 17:01:15 +0200

    kite_nrf9151: ncs 3.1.1 bump

--- boards/eub/kite_nrf9151/board.cmake
+++ boards/eub/kite_nrf9151/board.cmake
@@ -5,12 +5,13 @@ if(CONFIG_BOARD_KITE_NRF9151_NRF9151_NS)
 endif()
 
 if(CONFIG_TFM_FLASH_MERGED_BINARY)
-  set_property(TARGET runners_yaml_props_target PROPERTY hex_file "${CMAKE_BINARY_DIR}/tfm_merged.hex")
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
 endif()
 
 # FIXME: Does this even work
-board_runner_args(jlink "--device=nRF9161_xxCA" "--speed=4000")
+board_runner_args(jlink "--device=nRF9151_xxCA" "--speed=4000")
 board_runner_args(pyocd "--target=nrf9160_xxaa" "--frequency=4000000")
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
